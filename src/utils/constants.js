4b3ee3339068d746c096daf35eb3c1577acd624a
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2023-12-25 00:32:05 +0530

    create proper  structure, add hook

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/99/18/ac/9918accf-79cb-ed83-ca7a-b62482dfb028/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg";
